f8392933726c06572798136ff7c003725a8ac61d
Koji Murata <koji.murata@dena.com>
2017-09-01 19:20:03 +0900

    Added an project for carthage

---
+++ Carthage/PinCodeTextField/PinCodeTextField/PinCodeTextField.h
@@ -0,0 +1,19 @@
+//
+//  PinCodeTextField.h
+//  PinCodeTextField
+//
+//  Created by Koji Murata on 2017/09/01.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PinCodeTextField.
+FOUNDATION_EXPORT double PinCodeTextFieldVersionNumber;
+
+//! Project version string for PinCodeTextField.
+FOUNDATION_EXPORT const unsigned char PinCodeTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinCodeTextField/PublicHeader.h>
+
+
